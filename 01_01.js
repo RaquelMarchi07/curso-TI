d5f89ae07680e5d62257a0b355bc9d78229d5a89
RaquelMarchi07 <143852432+RaquelMarchi07@users.noreply.github.com>
2023-09-11 22:27:30 -0300

    1109

---
+++ 01_01.js
@@ -0,0 +1,12 @@
+const  = 20
+const2 = 10
+
+var pessoa1; console.log(pessoa "informe sua idade: ");d
+var pessoa2;
+
+ console.log(<= 18 "maior de idade" - "adulto paga ingresso inteiro" IntersectionObserver.);
+  console.log(>= 18 "menor de idade" - "adulto paga ingresso inteiro" IntersectionObserver.);
+  if pessoa1 prompt (<= 18 "você tem 18 anos de idade");
+  else{
+    pessoa1
+  }
